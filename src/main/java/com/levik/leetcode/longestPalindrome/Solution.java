dd3d1bc01df076d44eae104c55ee782a56fbac8d
levik <zhenya12@gmail.com>
2022-10-23 06:07:11 +0300

    add task desc longestPalindrome.

--- src/main/java/com/levik/leetcode/longestPalindrome/Solution.java
+++ src/main/java/com/levik/leetcode/longestPalindrome/Solution.java
@@ -1,8 +1,29 @@
 package com.levik.leetcode.longestPalindrome;
 
 import java.util.Objects;
-import java.util.stream.IntStream;
 
+/**
+ * 5. Longest Palindromic Substring (https://leetcode.com/problems/longest-palindromic-substring/)
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ *
+ * A string is called a palindrome string if the reverse of that string is the same as the original string.
+ *
+ * Example 1:
+ *
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * Example 2:
+ *
+ * Input: s = "cbbd"
+ * Output: "bb"
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ */
 public class Solution {
 
     public String longestPalindrome(String s) {
