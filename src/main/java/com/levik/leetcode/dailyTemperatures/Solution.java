d6acc87a29c35c821aa735d10aad68d6cb4e8386
Yevgen.P <yevgen.polukov@gmail.com>
2024-10-23 00:11:33 +0300

    implement Daily Temperatures.

--- src/main/java/com/levik/leetcode/dailyTemperatures/Solution.java
+++ src/main/java/com/levik/leetcode/dailyTemperatures/Solution.java
@@ -1,5 +1,7 @@
 package com.levik.leetcode.dailyTemperatures;
 
+import java.util.Stack;
+
 public class Solution {
 
   /**
@@ -18,6 +20,17 @@ public class Solution {
    * @return
    */
   public int[] dailyTemperatures(int[] temperatures) {
-      return new int[temperatures.length];
+    int[] res = new int[temperatures.length];
+    Stack<int[]> stack = new Stack<>(); // pair: [temp, index]
+
+    for (int i = 0; i < temperatures.length; i++) {
+      int t = temperatures[i];
+      while (!stack.isEmpty() && t > stack.peek()[0]) {
+        int[] pair = stack.pop();
+        res[pair[1]] = i - pair[1];
+      }
+      stack.push(new int[]{t, i});
+    }
+    return res;
   }
 }
